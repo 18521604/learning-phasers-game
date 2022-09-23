9c6ae4dc240679b920fbf75fc63441bdaf8be875
TuanNT88 <tuannt88@fpt.com.vn>
2022-09-23 11:02:43 +0700

    flappy-bird: change assets link

--- flappy-bird/src/scenes/preloadScene.ts
+++ flappy-bird/src/scenes/preloadScene.ts
@@ -10,15 +10,18 @@ export class PreloadScene extends Phaser.Scene {
 
     preload() {
         // this.load.setBaseURL("http://172.27.57.191:5501/flappy-bird");
-        this.load.image(ImagesScene.Sky, "assets/sky-2.png");
-        this.load.image(ImagesScene.Cloud, "assets/cloud-2.png");
-        this.load.spritesheet(ImagesScene.Bird, "assets/birdSprite.png", {
+        this.load.setBaseURL(
+            "https://hi-static.fpt.vn/sys/hifpt/pnc_pdx/game-demo/flappy-bird"
+        );
+        this.load.image(ImagesScene.Sky, "sky-2.png");
+        this.load.image(ImagesScene.Cloud, "cloud-2.png");
+        this.load.spritesheet(ImagesScene.Bird, "birdSprite.png", {
             frameWidth: 16,
             frameHeight: 16,
         });
-        this.load.image(ImagesScene.Pipe, "assets/pipe-3.png");
-        this.load.image(ImagesScene.Pause, "assets/pause.png");
-        this.load.image(ImagesScene.Back, "assets/back.png");
+        this.load.image(ImagesScene.Pipe, "pipe-3.png");
+        this.load.image(ImagesScene.Pause, "pause.png");
+        this.load.image(ImagesScene.Back, "back.png");
     }
 
     create() {
